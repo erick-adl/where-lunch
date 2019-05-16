48d3c7bc234810743cbda1eff673a60c50ab04cc
erick-rodosoft <erick@rodosoft.com.br>
2019-05-16 17:40:31 -0300

    add route user

---
+++ src/controllers/authController.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+
+router.post('/register', async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.send({ user });
+    } catch (error) {
+        res.status(400).send({ error: 'Registration failed' });
+    }
+});
+
+module.exports = app => app.use('/auth', router);
--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-app.get('/', function (req, res) {
-    res.send('Hello World again and again!');
-  });
+require('./controllers/authController')(app);
 
 app.listen(3000);
 
